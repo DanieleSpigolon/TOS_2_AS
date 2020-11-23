4b335caeb93060a7625128377777e97e0d9d6ac3
DanieleSpigolon <72398566+DanieleSpigolon@users.noreply.github.com>
2020-11-23 23:13:06 +0100

    Aggiunta class User e i relativi Test

---
+++ src/main/java/it/unipd/tos/model/User.java
@@ -0,0 +1,39 @@
+////////////////////////////////////////////////////////////////////
+// [DANIELE] [SPIGOLON] [1193290]
+////////////////////////////////////////////////////////////////////
+package it.unipd.tos.model;
+
+public class User {
+    
+    private String id;
+    private String nome;
+    private String cognome;
+    private int eta;
+    
+    public User(String id,String nome,String cognome,int eta)
+    {
+        this.id=id;
+        this.nome=nome;
+        this.cognome=cognome;
+        this.eta=eta;
+    }
+    public User(){
+        id=null;
+        nome=null;
+        cognome=null;
+        eta=0;
+    }
+    String getId(){
+        return id;
+    }
+    String getNome(){
+        return nome;
+    }
+    String getCognome(){
+        return cognome;
+    }
+    int getEta(){
+        return eta;
+    }
+}
+
